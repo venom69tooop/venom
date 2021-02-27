a0ffa06b6167238fab28158779e05671d13d3a9c
venom69tooop <79391904+venom69tooop@users.noreply.github.com>
2021-02-27 13:01:23 +0530

    Update _tagnotifs.py

--- plugins/_tagnotifs.py
+++ plugins/_tagnotifs.py
@@ -1,10 +1,3 @@
-# Ultroid - UserBot
-# Copyright (C) 2020 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
-
 from telethon import custom, events
 from telethon.utils import get_display_name
 
