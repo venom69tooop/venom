0cd44e5c1bb989e18cfa7ca28c48266fc3e97efc
venom69tooop <79391904+venom69tooop@users.noreply.github.com>
2021-02-27 13:44:21 +0530

    Update ssgen.py

--- resources/session/ssgen.py
+++ resources/session/ssgen.py
@@ -1,11 +1,3 @@
-#!/bin/bash
-# Ultroid - UserBot
-# Copyright (C) 2020 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
-
 from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
 
@@ -17,7 +9,7 @@ API_HASH = input("Enter API HASH: ")
 
 with TelegramClient(StringSession(), API_ID, API_HASH) as client:
     ult = client.send_message("me",f"`{client.session.save()}`")
-    ult.reply("The above is the `SESSION` for your current session.\nVisit @TheUltroid")
+    ult.reply("The above is the `SESSION` for your current session.")
     print("")
     print("String Session for the current login has been generated.")
     print("Check your Telegram Saved messages for your SESSION.")
