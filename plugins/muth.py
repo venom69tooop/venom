114756b7eab2b2b76f6819387190c8495f8fbfb9
venom69tooop <79391904+venom69tooop@users.noreply.github.com>
2021-03-01 13:40:58 +0530

    Update muth.py

--- plugins/muth.py
+++ plugins/muth.py
@@ -10,9 +10,10 @@ import asyncio
 
 
 
-@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
-
-async def _(event):
+@ultroid_cmd(
+    pattern="muth$",
+)
+async def cko(ult):
 
     if event.fwd_from:
 
@@ -23,8 +24,6 @@ async def _(event):
     animation_ttl = range(0, 100)
 
     input_str = event.pattern_match.group(1)
-
-    if input_str == "muth":
 
         await event.edit(input_str)
 
