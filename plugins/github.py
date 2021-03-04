cf2b17ca1f130a54272a8f26ae0b79b61ae35306
venom69tooop <79391904+venom69tooop@users.noreply.github.com>
2021-03-05 00:05:26 +0530

    Update github.py

--- plugins/github.py
+++ plugins/github.py
@@ -1,10 +1,3 @@
-# Ultroid - UserBot
-# Copyright (C) 2020 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
-
 """
 ✘ Commands Available -
 
@@ -67,4 +60,4 @@ async def gitsearch(event):
     )
 
 
-HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=Var.HNDLR)}"})
+HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=HNDLR)}"})
