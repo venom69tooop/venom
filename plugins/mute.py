affa31c233cc50fbaf81be4687b6b296280ce2eb
venom69tooop <79391904+venom69tooop@users.noreply.github.com>
2021-03-05 00:10:34 +0530

    Update mute.py

--- plugins/mute.py
+++ plugins/mute.py
@@ -1,10 +1,3 @@
-# Ultroid - UserBot
-# Copyright (C) 2020 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
-
 """
 ✘ Commands Available -
 
@@ -214,4 +207,4 @@ async def _(e):
         await eod(xx, f"`{str(m)}`")
 
 
-HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=Var.HNDLR)}"})
+HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=HNDLR)}"})
